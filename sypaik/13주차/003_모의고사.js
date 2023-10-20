6abdfda88d6536b0bf2446f93709f3614c83ce25
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-10-20 10:35:25 +0900

    ✨ feat: week_13-모의고사

--- sypaik/13주차/003_모의고사.js
+++ sypaik/13주차/003_모의고사.js
@@ -31,23 +31,22 @@ answers	return
 모든 사람이 2문제씩을 맞췄습니다.
 */
 function solution(answers) {
-
-    let arr1 = [1, 2, 3, 4, 5];
-    let arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    let arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
-    let scores = [0, 0, 0];
+    const one = [1, 2, 3, 4 ,5];
+    const two = [2, 1, 2, 3, 2, 4, 2, 5];
+    const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let score = [0, 0, 0];
     for(let i = 0; i < answers.length; i++) {
-        if(answers[i] == arr1[i % arr1.length]) scores[0]++
-        if(answers[i] == arr2[i % arr2.length]) scores[1]++
-        if(answers[i] == arr3[i % arr3.length]) scores[2]++
+        if (answers[i] === one[i % one.length]) score[0]++
+        if (answers[i] === two[i % two.length]) score[1]++
+        if (answers[i] === three[i % three.length]) score[2]++
     }
-    let maxScore = Math.max(...scores);
     let answer = [];
-    for(let i = 0; i < scores.length; i++) {
-        if(maxScore == scores[i]) {
-            answer.push(i+1)
-        } 
+    const max = Math.max(...score);
+    for(let i = 0; i < 3; i++) {
+        if(score[i] === max) {
+            answer.push(i + 1);
+        }
+        answer.sort((a, b) => a - b); // 이건 생략 가능
     }
-    return answer;
+    return answer
 }
